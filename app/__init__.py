4d7a44563587459510f29002b169aded789dc591
petegrace <petergrace84@gmail.com>
2019-01-14 22:27:54 +0000

    implement flask-talisman instead of sslify

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager, login_user
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_sslify import SSLify
+from flask_talisman import Talisman
 from config import Config
 import json
 
@@ -16,9 +16,28 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "auth.login"
 login.login_message = None
-sslify = SSLify(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+# TODO: Make these more specific and avoid the inline stuff
+csp = {
+    'default-src': [
+        '*'
+    ],
+    'style-src': [
+        '\'self\'',
+        '\'unsafe-inline\'',
+        '*'
+    ],
+    'img-src': '*',
+    'script-src': [
+        '\'self\'',
+        '\'unsafe-inline\'',
+        '\'unsafe-eval\'',
+        '*'
+    ]
+}
+talisman = Talisman(app, content_security_policy=csp)
 
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix="/auth")
