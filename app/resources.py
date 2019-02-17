52670b66a7eaaff1e44a6c6825c460d2bedf0a12
petegrace <petergrace84@gmail.com>
2019-02-17 15:22:04 +0000

    api to return 2019 stats for user

--- app/resources.py
+++ app/resources.py
@@ -1,30 +1,31 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import User
 
 class AnnualStats(Resource):
     @jwt_required
     def get(self):
+        email = get_jwt_identity()
+        current_user = User.query.filter_by(email=email).first()
+
+        counters = []
+        for activity_type_stat in current_user.current_year_activity_stats().all():
+            counters.append({
+                "category_name": activity_type_stat.activity_type,
+                "category_key": activity_type_stat.category_key,
+                "value": str(activity_type_stat.total_distance),
+                "uom": "km"
+            })
+        
+        for exercise_category_stat in current_user.current_year_exercise_stats().all():
+            counters.append({
+                "category_name": exercise_category_stat.category_name,
+                "category_key": exercise_category_stat.category_key,
+                "value": str(exercise_category_stat.total_sets),
+                "uom": "sets"
+            })
+
         return {
             "heading": "Your 2019 Stats",
-            "counters": [{
-                "category_name": "Run",
-                "category_key": "cat_blue",
-                "value": 51,
-                "uom": "km"
-            }, {
-                "category_name": "Strength",
-                "category_key": "cat_green",
-                "value": 52,
-                "uom": "sets"
-            }, {
-                "category_name": "Stretch",
-                "category_key": "cat_green_outline",
-                "value": 9,
-                "uom": "sets"
-            }, {
-                "category_name": "Balance",
-                "category_key": "cat_red",
-                "value": 35,
-                "uom": "sets"
-            }]
+            "counters": counters
         }
